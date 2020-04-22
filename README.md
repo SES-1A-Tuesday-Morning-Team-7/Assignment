4adeb35dbda4f9cab249199d58b0b6fa2a42ac78
Luke <13591476@student.uts.edu.au>
2020-04-22 14:48:00 +1000

    Login With JS No New Windows
    Login logic completed with non-persistent object array - database in progress.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Assignment
+# Assignment - Software Engineering Studio 1A - Autumn 2020 - Team 7 - Kenneth Hong, Luke Phillips, Sacha Gilchrist, Tien Duc Trieu, Jeffrey Ma, Etta Wu.
---
+++ root/js/index.js
@@ -0,0 +1,73 @@
+//Index login and registration page script. V0.1
+//
+//
+
+
+var fname, lname, email, user, pass;       //variables used in patRegistration() to hold text box values
+
+var patients = [];    //declaration of patients array of objects created on registration window.
+          
+var doctors = [         //dummy doctors array for testing purpose. Replace later with registration.
+    {
+        username: "mark",
+        password: "doc101"
+    },
+    {
+        username: "john",
+        password: "doc202"
+    }
+];
+
+function Patient(fname, lname, email, user, pass) {
+    this.fname = fname;
+    this.lname = lname;
+    this.email = email;
+    this.user = user;
+    this.pass = pass;
+}
+
+function patRegistration() {
+    fname = document.getElementById("fname").value;
+    lname = document.getElementById("lname").value;
+    email = document.getElementById("email").value;
+    user = document.getElementById("reg-user").value;
+    pass = document.getElementById("reg-pass").value;
+    patients.push(new Patient(fname, lname, email, user, pass));
+    alert("Thankyou for registering " + patients[patients.length - 1].fname + " you may now log in.");
+}
+
+function docLogin() {
+    var username = document.getElementById("doc-log-user").value;
+    var password = document.getElementById("doc-log-pass").value;
+    
+    for (i = 0; i < doctors.length; i++) {
+        if(username == doctors[i].username && password == doctors[i].password) {
+            console.log(doctors[i].username);
+            //
+            //
+            // Insert open new window and parameter passing here.
+            //
+            //
+            return
+        }
+    }
+    alert("Incorrect credentials, please try again.");
+}
+
+function patLogin() {
+    var username = document.getElementById("pat-log-user").value;
+    var password = document.getElementById("pat-log-pass").value;
+    
+    for (i = 0; i < patients.length; i++) {
+        if(username == patients[i].user && password == patients[i].pass) {
+            console.log(patients[i].user);
+            //
+            //
+            // Insert open new window and parameter passing here.
+            //
+            //
+            return
+        }
+    }
+    alert("Incorrect credentials, please try again.");
+}
