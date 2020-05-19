2596e716e562aa99c3cb6f787bab42b6da0419ed
Luke <13591476@student.uts.edu.au>
2020-05-19 13:15:52 +1000

    Doctor Create Prescription Complete

---
+++ root/js/doctor-prescription.js
@@ -0,0 +1,36 @@
+//Doctor Prescription Booking
+
+/* Required Variable Declarations and Database Pulls */
+var doctor = JSON.parse(window.localStorage.getItem("user")); 
+if(window.localStorage.getItem("scripts") === null) { 
+    var scripts = [];
+}
+else {
+    var scripts = JSON.parse(window.localStorage.getItem("scripts")); 
+}
+
+/* Prescrption Constructor */
+function Prescription(inputPat, inputDrug, inputDosage, inputDuration) {
+    this.doc = doctor.fname;
+    this.pat = inputPat;
+    this.drug = inputDrug;
+    this.dosage = inputDosage;
+    this.duration = inputDuration;
+}
+
+
+function newPrescription() {
+    var patName = document.getElementById("patNameField").value;
+    var drugName = document.getElementById("drugNameField").value;
+    var dosage = document.getElementById("dosageField").value;
+    var duration = document.getElementById("durationField").value;
+    //var script = new Prescription(patName, drugName, dosage, duration);
+    
+    scripts.push(new Prescription(patName, drugName, dosage, duration));
+    alert("Your prescription has been generated.");
+}
+
+function logOut() {
+    localStorage.setItem("scripts", JSON.stringify(scripts));     //set the current appointments, may need to delete current entry first, TEST THIS
+    window.open('../root/index.html', '_self');
+}
