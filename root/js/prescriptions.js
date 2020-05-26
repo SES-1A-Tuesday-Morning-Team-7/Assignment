eca11a1e5c6d5836c5a63ed7c25639cbcd754215
Sacha Gilchrist <sacha.gilchrist@gmail.com>
2020-05-26 11:32:31 +1000

    Prescriptions

---
+++ root/js/prescriptions.js
@@ -0,0 +1,48 @@
+var doctors = [         //dummy prescriptions array for testing purpose. Replace later with registration.
+    {
+        doc: "27/04/20 - David",
+        pat: "James",
+        drug: "Benzodiazepine",
+	dosage: "2ml a day",
+        duration: "2 weeks"
+    },
+    {
+        doc: "28/04/20 - David",
+        pat: "James",
+        drug: "Hydromorphone",
+	dosage: "3ml a day",
+        duration: "1 week"
+    }
+];
+
+function backBtn() {
+    window.open('patient-dash.html', '_self');
+}
+
+function viewPrescriptions() {
+	for (i = 0; i < doctors.length; i++) {
+		//DISPLAY NAME
+		var h2 = document.createElement("h2");
+		h2.className = "sub-text";
+  		var h2Text = document.createTextNode(doctors[i].doc);
+   		h2.appendChild(h2Text);
+
+		//DISPLAY DETAILS
+		var p = document.createElement("p");
+		p.className = "sub-text";
+  		var pText = document.createTextNode("Rx: " + doctors[i].drug
+							+ "\nDosage: " + doctors[i].dosage
+							+ "\nDuration: " + doctors[i].duration);
+   		p.appendChild(pText);
+
+		//ADD TO HTML
+
+		var div = document.createElement("div");
+		div.className = "prescription";
+		div.appendChild(h2);
+		div.appendChild(p);
+		
+   		var element = document.getElementById("prescriptionList");
+   		element.appendChild(div);
+	}
+}
